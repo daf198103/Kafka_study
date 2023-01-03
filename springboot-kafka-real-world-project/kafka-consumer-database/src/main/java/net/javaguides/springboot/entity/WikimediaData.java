bea7f15e48a7907ae719c34c36630441b7cd48ec
David França <david.franca01@fatec.sp.gov.br>
2023-01-03 12:39:53 -0300

    Add files via upload

---
+++ springboot-kafka-real-world-project/kafka-consumer-database/src/main/java/net/javaguides/springboot/entity/WikimediaData.java
@@ -0,0 +1,29 @@
+package net.javaguides.springboot.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "wikimedia_recentchange")
+@Getter
+@Setter
+public class WikimediaData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @Column(name="wiki_event_data")
+    private String wikiEventData;
+
+
+}
